57713e16161ab2459fbb4d00eff1e02cda41e5be
Hassan Hammoud <hassan.hammoud754@gmail.com>
2022-06-13 19:22:21 +0200

    hassan

--- Main.py
+++ Main.py
@@ -1,120 +1,97 @@
-from tkinter import *
+import tkinter
 from functools import partial
+from typing import Optional
 
 
-class board:
+class Board:
     def __init__(self):
-        self.Board : list = [["1","2","3"],["4","5","6"],["7","8","9"]]
+        self.rows: list = []
+        self.reset()
 
     def reset(self):
-        self.Board = [["1","2","3"],["4","5","6"],["7","8","9"]]
+        self.rows = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+
+    def disable_buttons(self):
+        for i in range(3):
+            for j in range(3):
+                if self.rows[i][j] != "X" and self.rows[i][j] != "O":
+                    tkinter.Button(window, text="", height=8, width=20, state=tkinter.DISABLED,
+                                   font=("Courier", 16)).grid(column=j, row=i)
 
 
-class blok:
-    def __init__(self):
-        self.i = 0
-        self.j = 0
-        self.b = ""
+class Player:
+    def __init__(self, name, character):
+        self.name: str = name
+        if character not in ["X", "O"]:
+            raise ValueError("Character must be either X or O")
+        self.character: str = character
 
 
-class player:
-    def __init__(self):
-        self.name = ""
-        self.value = 0
-        self.CharValue = 'X'
-        self.isHisRound = True
-        self.winner = False
+class XOGame:
+    def __init__(self, player_1: Player, player_2: Player):
+        self.reset_game()
+        self.board: Board = Board()
+        self.player1: Player = player_1
+        self.player2: Player = player_2
+        self.current_player: Player = self.player1
+        self.winner: Optional[Player] = None
+        self._draw_board()
+
+    def click_button(self, i, j):
+        if self.board.rows[i][j] == "" and self.winner is None:
+            self.board.rows[i][j] = self.current_player.character
+            tkinter.Button(
+                window, text=self.current_player.character, height=8, width=20,
+                state=tkinter.DISABLED, font=("Courier", 16)).grid(column=j, row=i)
+
+            if self._check_win():
+                self.winner = self.current_player
+                self.board.disable_buttons()
+
+    def _check_win(self):
+        winning_combinations = [
+            [(0, 0), (0, 1), (0, 2)],
+            [(1, 0), (1, 1), (1, 2)],
+            [(2, 0), (2, 1), (2, 2)],
+            [(0, 0), (1, 0), (2, 0)],
+            [(0, 1), (1, 1), (2, 1)],
+            [(0, 2), (1, 2), (2, 2)],
+            [(0, 0), (1, 1), (2, 2)],
+            [(0, 2), (1, 1), (2, 0)]]
+
+        for combination in winning_combinations:
+            if (
+                    self.board.rows[combination[0][0]][combination[0][1]]
+                    == self.board.rows[combination[1][0]][combination[1][1]]
+                    == self.board.rows[combination[2][0]][combination[2][1]]):
+                if self.board.rows[combination[0][0]][combination[0][1]] != "":
+                    return True
+        return False
+
+    def reset_game(self):
+        self.board = Board()
+        self.current_player = self.player1
+        self.winner = None
+        self._draw_board()
+
+    def _draw_board(self):
+        for i in range(0, 3):
+            for j in range(0, 3):
+                tkinter.Button(
+                    window, text="", height=8, width=20,
+                    command=partial(self.click_button, i, j),
+                    font=("Courier", 16)).grid(column=j, row=i)
 
 
-window = Tk()
+window = tkinter.Tk()
 window.title("TIC TAC TOE Game By Abdelkarim El Halabi")
 window.geometry("875x792")
 
-myBoard = board()
+p1 = Player("Player 1", "X")
+p2 = Player("Player 2", "O")
 
-def changeText(i,j):
-    if p1.isHisRound:
-        for b in buttons:
-            if b.i == i and b.j == j:
-                 Button(window, text="X",height = 8,width = 20,state = DISABLED,font=("Courier",16)).grid(column = j,row = i)
-                 myBoard.Board[i][j] = "X"
-                 print(myBoard.Board)
-                 break
-        p1.isHisRound = False
-        p2.isHisRound = True
-        if win():
-            p1.winner = True
-            print("P1 win")
-            disableButtons()
+game = XOGame(p1, p2)
 
-    elif p2.isHisRound:
-         for b in buttons:
-            if b.i == i and b.j == j:
-                 Button(window, text="O",height = 8,width = 20,state = DISABLED,font=("Courier",16)).grid(column = j,row = i)
-                 myBoard.Board[i][j] = "O"
-                 print(myBoard.Board)
-                 break
-         p2.isHisRound = False
-         p1.isHisRound = True
-         if win():
-            p2.winner = True
-            print("P2 win")
-            disableButtons()
-
-def disableButtons():
-    for i  in range(0,3):
-        for j in range(0,3):
-            if myBoard.Board[i][j] != "X" and myBoard.Board[i][j] != "O":
-                Button(window, text="",height = 8,width = 20,state = DISABLED,font=("Courier",16)).grid(column = j,row = i)
-
-buttons = []
-
-p1 = player()
-p1.value = 1
-p1.name = "Player 1"
-
-p2 = player()
-p2.name = "Player 2"
-p2.value = 2
-p2.CharValue = "O"
-p2.isHisRound = False
-
-def win():
-    return(
-        myBoard.Board[0][0] == myBoard.Board[0][1] and myBoard.Board[0][0] == myBoard.Board[0][2] or
-        myBoard.Board[1][0] == myBoard.Board[1][1] and myBoard.Board[1][0] == myBoard.Board[1][2] or
-        myBoard.Board[2][0] == myBoard.Board[2][1] and myBoard.Board[2][0] == myBoard.Board[2][2] or
-
-        myBoard.Board[0][0] == myBoard.Board[1][0] and myBoard.Board[0][0] == myBoard.Board[2][0] or
-        myBoard.Board[0][1] == myBoard.Board[1][1] and myBoard.Board[0][1] == myBoard.Board[2][1] or
-        myBoard.Board[0][2] == myBoard.Board[1][2] and myBoard.Board[0][2] == myBoard.Board[2][2] or
-
-        myBoard.Board[0][0] == myBoard.Board[1][1] and myBoard.Board[1][1] == myBoard.Board[2][2] or
-        myBoard.Board[0][2] == myBoard.Board[1][1] and myBoard.Board[1][1] == myBoard.Board[2][0]
-    )
-
-
-def drawBoard():
-    for i in range(0,3):
-        for j in  range(0,3):
-            bl = blok()
-            bl.i = i
-            bl.j = j
-            bl.b = Button(window, text="",height = 8,width = 20,command=partial(changeText,bl.i,bl.j),font=("Courier",16)).grid(column = j,row = i)
-            #b = Button(window, text="",height = 8,width = 20,command=partial(changeText,bl.i,bl.j),font=("Courier",16)).grid(column = j,row = i)
-            buttons.append(bl)
-
-def resetGame():
-    buttons.clear()
-    p1.isHisRound = True
-    p1.winner = False
-    p2.isHisRound = False
-    p2.winner = False
-    window.update() 
-    myBoard.reset()
-    drawBoard()
-
-Button(window, text="Restart Game",height = 8,width = 20,command=resetGame).grid(column = 2,row = 14)
-drawBoard()
+tkinter.Button(window, text="Restart Game", height=8, width=20, command=reset_game).grid(column=2, row=14)
 window.resizable(False, False)
 window.mainloop()
